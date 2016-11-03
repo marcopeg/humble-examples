bdf09e118fecdc4c158871931bd0ebccf15d3248
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-11-03 13:33:05 +0100

    Boot script auto detect wordpress up and auto seeds it

--- wordpress-website/scripts/dev.sh
+++ wordpress-website/scripts/dev.sh
@@ -1,27 +1,35 @@
 
-PROJECT_URL=${3:-"http://localhost:$HUMBLE_PORT"}
+# HUMBLE_URL
+# may be defined by the environment file
+if [ -z "$HUMBLE_URL" ]; then
+    HUMBLE_URL=${3:-"http://localhost:$HUMBLE_PORT"}
+fi
+
+humble info
 
 echo ""
 echo ""
 echo "### Tearing down existing project..."
+echo ""
 humble down
 
 echo ""
 echo ""
 echo "### Build and boot new project..."
+echo ""
 humble up -d --build
 
 echo ""
 echo ""
-echo "Waiting for: $PROJECT_URL"
-HTTP_DATA=$(fetch "$PROJECT_URL")
+echo "Waiting for: $HUMBLE_URL"
+HTTP_DATA=$(fetch "$HUMBLE_URL")
 while [ "$HTTP_DATA" == "" ]; do
-    HTTP_DATA=$(fetch "$PROJECT_URL")
-    echo "Waiting for: $PROJECT_URL"
+    HTTP_DATA=$(fetch "$HUMBLE_URL")
+    echo "Waiting for: $HUMBLE_URL"
     sleep 2.5
 done
 
-IS_WP_INSTALL=$(indexOf "$DATA" "wp-core-ui language-chooser")
+IS_WP_INSTALL=$(indexOf "$HTTP_DATA" "wp-core-ui language-chooser")
 if [ "$IS_WP_INSTALL" != "-1" ]; then
     echo ""
     echo ""
@@ -33,10 +41,11 @@ fi
 echo ""
 echo ""
 echo "System is ready at:"
-echo "$PROJECT_URL"
+echo "$HUMBLE_URL"
 sleep 5
 
 echo ""
 echo ""
 echo "### Attaching to the logs:"
+echo ""
 humble logs -f
