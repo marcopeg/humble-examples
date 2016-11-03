91f00a17d945ce9b81e9ee1756ba136d33a7b916
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-11-03 13:09:42 +0100

    Wordpress dev script is waiting for system to boot and does auto seeding

--- wordpress-website/scripts/dev.sh
+++ wordpress-website/scripts/dev.sh
@@ -1,3 +1,42 @@
+
+PROJECT_URL=${3:-"http://localhost:$HUMBLE_PORT"}
+
+echo ""
+echo ""
+echo "### Tearing down existing project..."
 humble down
+
+echo ""
+echo ""
+echo "### Build and boot new project..."
 humble up -d --build
+
+echo ""
+echo ""
+echo "Waiting for: $PROJECT_URL"
+HTTP_DATA=$(fetch "$PROJECT_URL")
+while [ "$HTTP_DATA" == "" ]; do
+    HTTP_DATA=$(fetch "$PROJECT_URL")
+    echo "Waiting for: $PROJECT_URL"
+    sleep 2.5
+done
+
+IS_WP_INSTALL=$(indexOf "$DATA" "wp-core-ui language-chooser")
+if [ "$IS_WP_INSTALL" != "-1" ]; then
+    echo ""
+    echo ""
+    echo "Wordpress: install page detected,"
+    echo "Run seeding script..."
+    humble run seed
+fi
+
+echo ""
+echo ""
+echo "System is ready at:"
+echo "$PROJECT_URL"
+sleep 5
+
+echo ""
+echo ""
+echo "### Attaching to the logs:"
 humble logs -f
